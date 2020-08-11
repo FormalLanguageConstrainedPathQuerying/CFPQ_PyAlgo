4ffec498ceba14993fbbf0924349d5215efde076
Vadim Abzalov <st067798@student.spbu.ru>
2020-08-11 16:08:17 +0300

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from src.grammar.cnf_grammar import CnfGrammar
 from src.graph.label_graph import LabelGraph
 from src.utils.time_profiler import SimpleTimer
 from src.algo.matrix_base import matrix_base_algo
-from src.algo.single_source.single_source import SingleSourceAlgoBrute, SingleSourceAlgoSmart
+from src.algo.single_source.single_source import SingleSourceAlgoBrute, SingleSourceAlgoSmart, SingleSourceAlgoOpt
 
 g = LabelGraph.from_txt('deps/CFPQ_Data/data/WorstCase/Matrices/worstcase_128.txt')
 gr = CnfGrammar.from_cnf('deps/CFPQ_Data/data/WorstCase/Grammars/Brackets.cnf')
@@ -30,7 +30,7 @@ for i in sources_vertices:
     st.toc()
     sum += st.duration
 
-print(f'SingleSourceAlgoSmart: {sum}')
+print(f'SingleSourceAlgoSmart:\n{sum}')
 
 ss_ao = SingleSourceAlgoOpt(g, gr)
 
@@ -42,7 +42,7 @@ for i in sources_vertices:
     st.toc()
     sum += st.duration
 
-print(f'SingleSourceAlgoOpt: {sum}')
+print(f'SingleSourceAlgoOpt:\n{sum}')
 
 assert m.to_lists() == m1.to_lists(), 'Not equal!'
 assert m.to_lists() == m2.to_lists(), 'Not equal!'
