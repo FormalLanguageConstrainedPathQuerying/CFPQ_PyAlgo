b460bc0aa51f51dd7960c42f719d990076d4f091
viabzalov <vadim.i.abzalov@gmail.com>
2020-08-12 15:42:32 +0300

    Fixed bug in test_single_source_benchmark_total with run_suite

--- tests/single_source/bench_cfpq_data.py
+++ tests/single_source/bench_cfpq_data.py
@@ -10,10 +10,10 @@ from tests.suites import all_cfpq_data_test_cases
 def test_single_source_benchmark_total(graph, grammar, algo, chunk_size, benchmark):
     g = LabelGraph.from_txt(graph)
     gr = CnfGrammar.from_cnf(grammar)
-    a = algo(g, gr)
     chunks = g.chunkify(g.matrices_size if chunk_size is None else chunk_size)
 
     def run_suite():
+        a = algo(g, gr)
         for chunk in chunks:
             a.solve(chunk)
 
