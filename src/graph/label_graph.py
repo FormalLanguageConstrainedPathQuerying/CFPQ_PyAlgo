1024c7c219a6d6035d6596a1f7641e315917363b
Ilya Epelbaum <33521263+IlyaEp@users.noreply.github.com>
2020-09-17 15:27:35 +0300

    Replacing tabs

--- src/graph/label_graph.py
+++ src/graph/label_graph.py
@@ -9,7 +9,7 @@ class LabelGraph:
     def __init__(self, matrices_size, number_edges):
         self.matrices = {}
         self.matrices_size = matrices_size
-		self.number_edges = number_edges
+	self.number_edges = number_edges
 
     def __getitem__(self, item: str) -> Matrix:
         if item not in self.matrices:
@@ -22,32 +22,32 @@ class LabelGraph:
     def __iter__(self):
         return self.matrices.__iter__()
 
-	def get_number_of_vertices(self):
-		return self.matrices_size
+    def get_number_of_vertices(self):
+	return self.matrices_size
 
-	def get_number_of_edges(self):
-		return self.number_edges
+    def get_number_of_edges(self):
+	return self.number_edges
 
     @classmethod
     def from_txt(cls, path, verbose=False):
         triplets = list()
-		size_matrices = 0
-		number_edges = 0
+	size_matrices = 0
+	number_edges = 0
         with open(path, 'r') as f:
             for line in tqdm(f.readlines()) if verbose else f.readlines():
 
-				number_edges += 1
+		number_edges += 1
 				
                 v, label, to = line.split()
                 v, to = int(v), int(to)
                 size_matrices = max(size_matrices, v, to)
 				
-				triplets.append((v, label, to))
+		triplets.append((v, label, to))
 
 		g = LabelGraph(size_matrices + 1, number_edges)
 		
 		for triplet in triplets:
-			g[triplet[1]][triplet[0], triplet[2]] = True
+ 		    g[triplet[1]][triplet[0], triplet[2]] = True
 
         return g
 
