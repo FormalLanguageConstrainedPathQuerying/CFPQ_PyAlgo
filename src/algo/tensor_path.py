25a84f87ac78bf7e1116e32d7623c018a6396284
Ilya Epelbaum <33521263+IlyaEp@users.noreply.github.com>
2020-09-18 18:03:15 +0300

    Resolve

--- src/algo/tensor_path.py
+++ src/algo/tensor_path.py
@@ -1,21 +1,9 @@
 from pygraphblas import Matrix
 from more_itertools import unique_everseen
+from itertools import islice
 
 from src.grammar.rsa import RecursiveAutomaton
 from src.graph.label_graph import LabelGraph
-
-
-def get_elements(s: set, n):
-    count = 0
-    result = set()
-    for elem in s:
-        if count == n:
-            break
-        result.add(elem)
-        count += 1
-
-    return result
-
 
 class Paths:
 
@@ -91,24 +79,22 @@ class TensorPaths:
 
         self.tc = tc
 
-        self.tc_coord = {i[0] for i in tc}
-        for i in tc:
-            self.tc_coord.add(i[1])
-
         self.rsa_element = dict()
         for label in rsa.labels():
             self.rsa_element.update({label: {(i[0], i[1]) for i in rsa.automaton()[label]}})
 
-        self.graph_element = dict()
-        for label in graph:
-            self.graph_element.update({label: {(i[0], i[1]) for i in graph[label]}})
+        self.graph_element = graph
 
-    def GetPaths(self, v_s, v_f, N):
+    def new_launch(self):
+        self.exist_inner = set()
+        self.exist_inner = set()
+        self.last_count = 1
+        self.last_inner = (-1, -1)
+
+    def get_paths(self, v_s, v_f, N):
 
         if (v_s, v_f) in self.exist_paths:
             return Paths()
-
-        #print("Get paths (" + str(v_s) + ", " + str(v_f) + ")")
 
         self.exist_paths.add((v_s, v_f))
 
@@ -117,7 +103,6 @@ class TensorPaths:
 
         result = Paths()
         for f in f_N:
-
             check = True
             for label in self.rsa.labels().difference(self.rsa.S()):
                 if (v_s, v_f) in self.graph_element[label] and (q_N, f) in self.rsa_element[label]:
@@ -125,54 +110,41 @@ class TensorPaths:
                     if self.count_paths == 1:
                         check = False
             if check:
-                result.union_paths(self.GetPathsInner(q_N * self.size_graph + v_s, f * self.size_graph + v_f))
+                result.union_paths(self.get_paths_inner(q_N * self.size_graph + v_s, f * self.size_graph + v_f))
 
         self.exist_paths.remove((v_s, v_f))
-
-        #print("Get paths (" + str(v_s) + ", " + str(v_f) + ")")
-        #print("result = " + str(result))
 
         if not self.exist_paths:
             result.doSet()
 
         return result
 
-    def GetPathsInner(self, i, j):
-
-        #print("Get paths inner (" + str(i) + ", " + str(j) + ")")
+    def get_paths_inner(self, i, j):
 
         if (i, j) in self.exist_inner:
             return Paths()
 
         self.exist_inner.add((i, j))
 
-        parts = set()
+        parts = self.tc[i] * self.tc[:, j]
 
-        for elem in self.tc_coord:
-            if (i, elem) in self.tc and (elem, j) in self.tc:
-                parts.add(elem)
-
-        if len(parts) > 1:
-            dif = self.count_paths - len(parts) + 1 - self.last_count
+        if parts.nvals > 1:
+            dif = self.count_paths - parts.nvals + 1 - self.last_count
             if dif >= 0:
-                self.last_count += len(parts) - 1
+                self.last_count += parts.nvals - 1
             else:
-                parts = get_elements(parts, len(parts) + dif)
+                parts = islice(parts, parts.nvals + dif)
 
         result = Paths()
         for part in parts:
-            result.union_paths(self.GetSubPaths(i, j, part))
+            result.union_paths(self.get_sub_paths(i, j, part[0]))
 
         self.exist_inner.remove((i, j))
 
-        #print("Get paths inner (" + str(i) + ", " + str(j) + ")")
-        #print("result = " + str(result))
-
         return result
 
-    def GetSubPaths(self, i, j, k):
-        #print("get sub paths (" + str(i) + ", " + str(j) + ", " + str(k) + ")")
-
+    def get_sub_paths(self, i, j, k):
+        
         left = Paths()
         for label in self.rsa.labels().difference(self.rsa.S()):
             if (i % self.size_graph, k % self.size_graph) in self.graph_element[label] and (
@@ -183,12 +155,9 @@ class TensorPaths:
             if N not in self.rsa_element:
                 continue
             if (i // self.size_graph, k // self.size_graph) in self.rsa_element[N]:
-                left.union_paths(self.GetPaths(i % self.size_graph, k % self.size_graph, N))
+                left.union_paths(self.get_paths(i % self.size_graph, k % self.size_graph, N))
 
-        left.union_paths(self.GetPathsInner(i, k))
-
-        #print("get sub paths (" + str(i) + ", " + str(j) + ", " + str(k) + ")")
-        #print("left = " + str(left))
+        left.union_paths(self.get_paths_inner(i, k))
 
         right = Paths()
         for label in self.rsa.labels().difference(self.rsa.S()):
@@ -200,12 +169,9 @@ class TensorPaths:
             if N not in self.rsa_element:
                 continue
             if (k // self.size_graph, j // self.size_graph) in self.rsa_element[N]:
-                right.union_paths(self.GetPaths(k % self.size_graph, j % self.size_graph, N))
+                right.union_paths(self.get_paths(k % self.size_graph, j % self.size_graph, N))
 
-        right.union_paths(self.GetPathsInner(k, j))
-
-        #print("get sub paths (" + str(i) + ", " + str(j) + ", " + str(k) + ")")
-        #print("right = " + str(right))
+        right.union_paths(self.get_paths_inner(k, j))
 
         left.product_paths(right)
 
