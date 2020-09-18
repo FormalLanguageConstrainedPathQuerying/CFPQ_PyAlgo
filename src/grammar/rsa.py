b9c5d3c806ac515360261e90bafb9e4c0ea5db06
Ilya Epelbaum <33521263+IlyaEp@users.noreply.github.com>
2020-09-18 18:04:54 +0300

    Resolve

--- src/grammar/rsa.py
+++ src/grammar/rsa.py
@@ -8,12 +8,24 @@ class RecursiveAutomaton:
         self._S = set()
         self._automaton = dict()
         self._states = dict()
+        self._start_and_finish = set()
         self._matrices_size = 0
         self._count_s = 0
         self._count_automaton = 0
+        self._start_state = dict()
+        self._finish_states = dict()
+
+    def start_state(self):
+        return self._start_state
+
+    def finish_states(self):
+        return self._finish_states
 
     def labels(self):
         return self._labels
+
+    def start_and_finish(self):
+        return self._start_and_finish
 
     def S(self):
         return self._S
@@ -56,27 +68,37 @@ class RecursiveAutomaton:
         self._S.add(label)
         self._states.update({label: Matrix.sparse(BOOL, self._matrices_size, self._matrices_size)})
 
+    @classmethod
     def from_file(self, path: str):
+        rsa = RecursiveAutomaton()
         with open(path, "r") as file:
             count_matrix = int(file.readline())
-            self.change_count_matrix(count_matrix)
+            rsa.change_count_matrix(count_matrix)
             count_s = int(file.readline())
-            self.change_count_s(count_s)
+            rsa.change_count_s(count_s)
             size_matrix = int(file.readline())
-            self.change_size(size_matrix)
+            rsa.change_size(size_matrix)
 
             for i in range(count_matrix):
                 label = file.readline().replace("\n", "")
-                self.add_automaton(label)
+                rsa.add_automaton(label)
                 count_edge = int(file.readline())
                 for j in range(count_edge):
                     first, second = file.readline().split()
-                    self._automaton[label][int(first), int(second)] = True
+                    rsa._automaton[label][int(first), int(second)] = True
 
             for i in range(count_s):
                 label = file.readline().replace("\n", "")
-                self.add_states(label)
+                rsa.add_states(label)
                 count_edge = int(file.readline())
                 for j in range(count_edge):
                     first, second = file.readline().split()
-                    self._states[label][int(first), int(second)] = True
+                    rsa._states[label][int(first), int(second)] = True
+                    rsa._start_state.update({label: int(first)})
+                    if label in rsa._finish_states:
+                        rsa._finish_states[label].append(int(second))
+                    else:
+                        rsa._finish_states.update({label: [int(second)]})
+                    if first == second:
+                        rsa._start_and_finish.add(label)
+        return rsa
