c56e4ea28ab528ba2401986a6186f5b1c3f19843
Ilya Epelbaum <iliyepelbaun@gmail.com>
2020-11-09 13:33:04 +0300

    Add common interface for tensor

--- src/algo/tensor/tensor.py
+++ src/algo/tensor/tensor.py
@@ -1,74 +1,84 @@
 from pygraphblas import *
+from abc import ABC
 
 from src.graph.label_graph import LabelGraph
 from src.grammar.rsa import RecursiveAutomaton
 
 
-def tensor_algo(graph: LabelGraph, grammar: RecursiveAutomaton):
-    for start in grammar.S():
-        graph.matrices.update({start: Matrix.sparse(BOOL, graph.matrices_size, graph.matrices_size)})
-
-    for label in grammar.start_and_finish():
-        for i in range(graph.matrices_size):
-            graph[label][i, i] = True
-
-    sizeKron = graph.matrices_size * grammar.matrices_size()
-
-    kron = Matrix.sparse(BOOL, sizeKron, sizeKron)
-    control_sum = 0
-    count_tc = 0
-    first_iter = True
-    changed = True
-    while changed:
-        changed = False
-
-        # calculate kronecker
-        for label in grammar.labels():
-            with semiring.LOR_LAND_BOOL:
-                kron += grammar.automaton()[label].kron(graph[label])
-
-        kron.select(lib.GxB_NONZERO)
-
-        # transitive closer
-        prev = kron.nvals
-        degree = kron
-        transitive_changed = True
-        while transitive_changed:
-            if first_iter:
-                count_tc += 1
-            transitive_changed = False
-            with semiring.LOR_LAND_BOOL:
-                degree = degree @ kron
-            degree.select(lib.GxB_NONZERO)
-            with semiring.LOR_LAND_BOOL:
-                kron += degree
-            cur = kron.nvals
-            if prev != cur:
-                prev = cur
-                transitive_changed = True
-
-        first_iter = False
-
-        kron.select(lib.GxB_NONZERO)
-
-        # update
+class TensorSolver(ABC):
+    def __init__(self, graph: LabelGraph, grammar: RecursiveAutomaton):
+        self.graph = graph
+        self.grammar = grammar
         for start in grammar.S():
-            for element in grammar.states()[start]:
-                i = element[0]
-                j = element[1]
+            graph.matrices.update({start: Matrix.sparse(BOOL, graph.matrices_size, graph.matrices_size)})
 
-                start_i = i * graph.matrices_size
-                start_j = j * graph.matrices_size
-                block = kron[start_i:start_i + graph.matrices_size - 1, start_j: start_j + graph.matrices_size - 1]
-                block.select(lib.GxB_NONZERO)
 
+class TensorAlgoSimple(TensorSolver):
+    def __init__(self, graph: LabelGraph, grammar: RecursiveAutomaton):
+        super().__init__(graph, grammar)
+
+    def solve(self):
+        for label in self.grammar.start_and_finish():
+            for i in range(self.graph.matrices_size):
+                self.graph[label][i, i] = True
+
+        sizeKron = self.graph.matrices_size * self.grammar.matrices_size()
+
+        kron = Matrix.sparse(BOOL, sizeKron, sizeKron)
+        control_sum = 0
+        count_tc = 0
+        first_iter = True
+        changed = True
+        while changed:
+            changed = False
+
+            # calculate kronecker
+            for label in self.grammar.labels():
                 with semiring.LOR_LAND_BOOL:
-                    graph[start] += block
-                    graph[start].select(lib.GxB_NONZERO)
-                new_control_sum = graph[start].nvals
+                    kron += self.grammar.automaton()[label].kron(self.graph[label])
 
-                if new_control_sum != control_sum:
-                    changed = True
-                    control_sum = new_control_sum
+            kron.select(lib.GxB_NONZERO)
 
-    return control_sum, graph, kron, count_tc
+            # transitive closer
+            prev = kron.nvals
+            degree = kron
+            transitive_changed = True
+            while transitive_changed:
+                if first_iter:
+                    count_tc += 1
+                transitive_changed = False
+                with semiring.LOR_LAND_BOOL:
+                    degree = degree @ kron
+                degree.select(lib.GxB_NONZERO)
+                with semiring.LOR_LAND_BOOL:
+                    kron += degree
+                cur = kron.nvals
+                if prev != cur:
+                    prev = cur
+                    transitive_changed = True
+
+            first_iter = False
+
+            kron.select(lib.GxB_NONZERO)
+
+            # update
+            for start in self.grammar.S():
+                for element in self.grammar.states()[start]:
+                    i = element[0]
+                    j = element[1]
+
+                    start_i = i * self.graph.matrices_size
+                    start_j = j * self.graph.matrices_size
+                    block = kron[start_i:start_i + self.graph.matrices_size - 1, start_j: start_j + self.graph.matrices_size - 1]
+                    block.select(lib.GxB_NONZERO)
+
+                    with semiring.LOR_LAND_BOOL:
+                        self.graph[start] += block
+                        self.graph[start].select(lib.GxB_NONZERO)
+                    new_control_sum = self.graph[start].nvals
+
+                    if new_control_sum != control_sum:
+                        changed = True
+                        control_sum = new_control_sum
+
+        return control_sum, self.graph, kron, count_tc
--- test/tensor/tensor.py
+++ test/tensor/tensor.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.algo.tensor.tensor import tensor_algo
+from src.algo.tensor.tensor import TensorAlgoSimple
 from src.grammar.rsa import RecursiveAutomaton
 from src.utils.useful_paths import LOCAL_CFPQ_DATA
 from src.graph.label_graph import LabelGraph
@@ -11,30 +11,36 @@ def test_case_cycle():
     path = LOCAL_CFPQ_DATA.joinpath('tensor/test_cycle')
     graph = LabelGraph.from_txt(path.joinpath('graph_cycle'))
     rsa = RecursiveAutomaton.from_file(path.joinpath('rsa_cycle'))
-    control_sum, _, _, _ = tensor_algo(graph, rsa)
+    algo = TensorAlgoSimple(graph, rsa)
+    control_sum, _, _, _ = algo.solve()
     assert control_sum == 9
+
 
 @pytest.mark.CI
 def test_case_loop():
     path = LOCAL_CFPQ_DATA.joinpath('tensor/test_loop')
     graph = LabelGraph.from_txt(path.joinpath('graph_loop'))
     rsa = RecursiveAutomaton.from_file(path.joinpath('rsa_loop'))
-    control_sum, _, _, _ = tensor_algo(graph, rsa)
+    algo = TensorAlgoSimple(graph, rsa)
+    control_sum, _, _, _ = algo.solve()
     assert control_sum == 3
+
 
 @pytest.mark.CI
 def test_case_rpq():
     path = LOCAL_CFPQ_DATA.joinpath('tensor/test_rpq')
     graph = LabelGraph.from_txt(path.joinpath('graph_rpq'))
     rsa = RecursiveAutomaton.from_file(path.joinpath('rsa_rpq'))
-    control_sum, _, _, _ = tensor_algo(graph, rsa)
+    algo = TensorAlgoSimple(graph, rsa)
+    control_sum, _, _, _ = algo.solve()
     assert control_sum == 5
+
 
 @pytest.mark.CI
 def test_case_simple():
     path = LOCAL_CFPQ_DATA.joinpath('tensor/test_simple')
     graph = LabelGraph.from_txt(path.joinpath('graph_simple'))
     rsa = RecursiveAutomaton.from_file(path.joinpath('rsa_simple'))
-    control_sum, _, _, _ = tensor_algo(graph, rsa)
+    algo = TensorAlgoSimple(graph, rsa)
+    control_sum, _, _, _ = algo.solve()
     assert control_sum == 2
-
