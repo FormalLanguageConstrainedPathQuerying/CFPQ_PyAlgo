ac5a3b50b797c20998a48a33decd321dae993033
Ilya Epelbaum <iliyepelbaun@gmail.com>
2021-01-24 18:40:04 +0300

    Add new version for paths

--- src/algo/tensor/tensor.py
+++ src/algo/tensor/tensor.py
@@ -26,40 +26,26 @@ class TensorAlgoSimple(TensorSolver):
 
         kron = Matrix.sparse(BOOL, sizeKron, sizeKron)
         control_sum = 0
-        count_tc = 0
-        first_iter = True
         changed = True
         while changed:
             changed = False
 
             # calculate kronecker
             for label in self.grammar.labels():
-                with semiring.LOR_LAND_BOOL:
-                    kron += self.grammar.automaton()[label].kron(self.graph[label])
-
-            kron.select(lib.GxB_NONZERO)
+                kron += self.grammar.automaton()[label].kron(self.graph[label])
 
             # transitive closer
             prev = kron.nvals
             degree = kron
             transitive_changed = True
             while transitive_changed:
-                if first_iter:
-                    count_tc += 1
                 transitive_changed = False
-                with semiring.LOR_LAND_BOOL:
-                    degree = degree @ kron
-                degree.select(lib.GxB_NONZERO)
-                with semiring.LOR_LAND_BOOL:
-                    kron += degree
+                degree = degree @ kron
+                kron += degree
                 cur = kron.nvals
                 if prev != cur:
                     prev = cur
                     transitive_changed = True
-
-            first_iter = False
-
-            kron.select(lib.GxB_NONZERO)
 
             # update
             for start in self.grammar.S():
@@ -69,16 +55,14 @@ class TensorAlgoSimple(TensorSolver):
 
                     start_i = i * self.graph.matrices_size
                     start_j = j * self.graph.matrices_size
-                    block = kron[start_i:start_i + self.graph.matrices_size - 1, start_j: start_j + self.graph.matrices_size - 1]
-                    block.select(lib.GxB_NONZERO)
+                    block = kron[start_i:start_i + self.graph.matrices_size - 1,
+                            start_j: start_j + self.graph.matrices_size - 1]
 
-                    with semiring.LOR_LAND_BOOL:
-                        self.graph[start] += block
-                        self.graph[start].select(lib.GxB_NONZERO)
+                    self.graph[start] += block
                     new_control_sum = self.graph[start].nvals
 
                     if new_control_sum != control_sum:
                         changed = True
                         control_sum = new_control_sum
 
-        return control_sum, self.graph, kron, count_tc
+        return control_sum, self.graph, kron
