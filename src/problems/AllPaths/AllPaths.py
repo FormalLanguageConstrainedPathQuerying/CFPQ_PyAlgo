b4fd5cefedf6720c98c98ab655d4b1e2f2d07263
Ilya Ep <epelphd@gmail.com>
2021-05-15 11:39:38 +0300

    Add doc for problems

--- src/problems/AllPaths/AllPaths.py
+++ src/problems/AllPaths/AllPaths.py
@@ -9,12 +9,27 @@ class AllPathsProblem(ABC):
 
     @abstractmethod
     def prepare(self, graph: Path, grammar: Path):
+        """
+        Prepare for the operation of the algorithm: load graph and grammar
+        @param graph: path to file with graph
+        @param grammar: path to file with grammar
+        """
         pass
 
     @abstractmethod
     def solve(self):
+        """
+        Solve problem with graph and grammar
+        """
         pass
 
     @abstractmethod
     def getPaths(self, v_start: int, v_finish: int, nonterminal: str, max_len: int):
+        """
+        Extract all paths between two vertices, the length of which does not exceed the given parameter
+        @param v_start: starting vertex for paths
+        @param v_finish: finishing vertex for paths
+        @param nonterminal: nonterminal from which paths are being restored
+        @param max_len: path length limitation
+        """
         pass
