83dc3d23134328c8d280abd98de1b3939b15c4a4
Ilya Epelbaum <iliyepelbaun@gmail.com>
2021-05-17 09:47:29 +0300

    Fix conftest for tests

--- test/SinglePath/conftest.py
+++ test/SinglePath/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 
-from src.problems.SinglePath.algo.matrix_single_path.matrix_single_path import MatrixSinglePath
+from src.problems.SinglePath.algo.matrix_single_path.matrix_single_path_index import MatrixSingleAlgo
 
 
-@pytest.fixture(params=[MatrixSinglePath])
+@pytest.fixture(params=[MatrixSingleAlgo])
 def algo(request):
     return request.param
