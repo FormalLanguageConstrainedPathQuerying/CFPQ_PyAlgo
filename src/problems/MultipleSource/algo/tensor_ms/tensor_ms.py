76bfc8dbfc6f65e6e5c9c0201c5a55b4101ba86f
Ilya Ep <epelphd@gmail.com>
2021-06-07 09:58:31 +0300

    Add new version of tensor ms with extract all subgraph

--- src/problems/MultipleSource/algo/tensor_ms/tensor_ms.py
+++ src/problems/MultipleSource/algo/tensor_ms/tensor_ms.py
@@ -90,3 +90,82 @@ class TensorMSAlgo(MultipleSourceProblem):
                         changed = True
 
         return ResultAlgo(self.graph[self.grammar.start_nonterm], iter)
+
+
+class TensorMSAllAlgo(MultipleSourceProblem):
+
+    def prepare(self, graph: Graph, grammar: CFG):
+        self.graph = graph
+        self.graph.load_bool_graph()
+        self.grammar = RecursiveAutomaton.from_cfg(grammar)
+        self.part_graph = LabelGraph(self.graph.matrices_size)
+        self.src_for_states = dict()
+        for i in range(self.grammar.matrices_size):
+            self.src_for_states.update({i: Matrix.sparse(BOOL, self.graph.matrices_size, self.graph.matrices_size)})
+
+    def clear_src(self):
+        for label in self.src_for_states:
+            self.src_for_states[label].clear()
+
+        for nonterm in self.grammar.nonterminals:
+            self.graph[nonterm].clear()
+
+    def solve(self, sources: Iterable):
+        restore_eps_paths(self.grammar.start_and_finish, self.graph)
+
+        for v in sources:
+            self.src_for_states[self.grammar.start_state[self.grammar.start_nonterm]][v, v] = True
+
+        sizeKron = self.graph.matrices_size * self.grammar.matrices_size
+
+        kron = Matrix.sparse(BOOL, sizeKron, sizeKron)
+
+        changed = True
+        iter = 0
+        while changed:
+            iter += 1
+            changed = False
+            src_changed = True
+            while src_changed:
+                src_changed = False
+                for box in self.grammar.boxes:
+                    for state in self.grammar.boxes[box]:
+                        out_state = self.grammar.out_states.get(state, [])
+                        for out in out_state:
+                            if out[1] in self.grammar.nonterminals:
+                                old_sum = self.src_for_states[self.grammar.start_state[out[1]]].nvals
+                                self.src_for_states[self.grammar.start_state[out[1]]] += self.src_for_states[state]
+                                if old_sum != self.src_for_states[self.grammar.start_state[out[1]]].nvals:
+                                    src_changed = True
+                            with BOOL.LOR_LAND:
+                                self.part_graph[out[1]] += self.src_for_states[state].mxm(self.graph[out[1]])
+                            old_sum = self.src_for_states[out[0]].nvals
+                            for elem in self.part_graph[out[1]].T.reduce_vector(BOOL.LAND_MONOID):
+                                self.src_for_states[out[0]][elem[0], elem[0]] = True
+                            if old_sum != self.src_for_states[out[0]].nvals:
+                                src_changed = True
+
+            for label in self.grammar.labels:
+                kron += self.grammar[label].kronecker(self.part_graph[label])
+
+            transitive_closure(kron)
+
+            for start in self.grammar.nonterminals:
+                for element in self.grammar.states[start]:
+                    i = element[0]
+                    j = element[1]
+
+                    start_i = i * self.graph.matrices_size
+                    start_j = j * self.graph.matrices_size
+
+                    control_sum = self.graph[start].nvals
+                    block = kron[start_i:start_i + self.graph.matrices_size - 1,
+                                 start_j: start_j + self.graph.matrices_size - 1]
+
+                    self.graph[start] += block
+                    new_control_sum = self.graph[start].nvals
+
+                    if new_control_sum != control_sum:
+                        changed = True
+
+        return ResultAlgo(self.graph[self.grammar.start_nonterm], iter)
