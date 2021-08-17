71768d9f97efa3dc4c0b53e07263fa46eb010cff
vdshk <vadim.i.abzalov@gmail.com>
2021-08-17 14:56:03 +0300

    Removed `test`

--- test/Base/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from src.problems.Base.algo.matrix_base.matrix_base import MatrixBaseAlgo
-
-
-@pytest.fixture(params=[MatrixBaseAlgo])
-def algo(request):
-    return request.param
