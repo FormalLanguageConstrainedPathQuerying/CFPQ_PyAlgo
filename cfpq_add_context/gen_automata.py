5b16c8d20ce98a6a5086eaa55cb552243797e569
gsv <rsdpisuy@gmail.com>
2025-09-24 10:43:27 +0300

    [EVALUATION] Automata for depth 1.

--- cfpq_add_context/gen_automata.py
+++ cfpq_add_context/gen_automata.py
@@ -8,11 +8,12 @@ from cfpq_add_context.utils import print_matrix_to_dot
 
 def generate (number_of_contexts):
     start = 0
-    nvertices = number_of_contexts + number_of_contexts * number_of_contexts + 1
+    #nvertices = number_of_contexts + number_of_contexts * number_of_contexts + 1
+    nvertices = number_of_contexts + 1
     final = nvertices
     first_level = [i for i in range(1, number_of_contexts + 1)]
     #print("First level: ", first_level)
-    second_level = [i for i in range(number_of_contexts + 1, nvertices)]
+    #second_level = [i for i in range(number_of_contexts + 1, nvertices)]
     def mk_open_context(i):
             return mk_open_context_from_pass(i % number_of_contexts)
 
@@ -26,19 +27,21 @@ def generate (number_of_contexts):
                 for on_first_level in first_level 
                 for edg in ((start, on_first_level, mk_open_context(on_first_level - 1)),(on_first_level, start, mk_close_context(on_first_level - 1)))
              ] + 
-             [
-                edg 
-                for on_first_level in first_level 
-                for on_second_level in range(on_first_level * number_of_contexts + 1, on_first_level * number_of_contexts + number_of_contexts + 1) 
-                for edg in ((on_first_level, on_second_level, mk_open_context(on_second_level % number_of_contexts )),(on_second_level, on_first_level, mk_close_context(on_second_level % number_of_contexts )))
-             ] +
-             [(i,i,SIGMA_WITHOUT_CONTEXTS) for i in first_level + second_level] +
-             [(i,final,ALL_OPEN_CONTEXTS) for i in second_level]
+             #[
+             #   edg 
+             #   for on_first_level in first_level 
+             #   for on_second_level in range(on_first_level * number_of_contexts + 1, on_first_level * number_of_contexts + number_of_contexts + 1) 
+             #   for edg in ((on_first_level, on_second_level, mk_open_context(on_second_level % number_of_contexts )),(on_second_level, on_first_level, mk_close_context(on_second_level % number_of_contexts )))
+             #] +
+             #[(i,i,SIGMA_WITHOUT_CONTEXTS) for i in first_level + second_level] +
+             #[(i,final,ALL_OPEN_CONTEXTS) for i in second_level]
+             [(i,i,SIGMA_WITHOUT_CONTEXTS) for i in first_level] +
+             [(i,final,ALL_OPEN_CONTEXTS) for i in first_level]
              
              )
     
     result = Matrix.from_edgelist(edges, dtype=UINT64, nrows=nvertices + 1, ncols=nvertices + 1, name="automata")
     
-    #print_matrix_to_dot(result, "atm.dot")
+    print_matrix_to_dot(result, "atm.dot")
 
     return result
